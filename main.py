b6f9811d73980ded45d733201f2320a56f5a6b13
unknown <ma.munozc@alum.up.edu.pe>
2024-08-19 16:12:33 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+frase=input("Ingrese la palabra: ")
+def changediccionario(lista):
+    L1=list(lista.lower())
+    cont = 0
+    for i in range(len(L1)):
+        for j in range(i,len(L1)):
+            if (L1[i]==L1[j]):
+                cont = cont + 1
+    return cont
+print(changediccionario(frase))
